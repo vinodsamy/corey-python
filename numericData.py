ff1dbd69d32e77e247f82a3285c580113937c30f
vinodsamy <vino929510@gmail.com>
2021-10-24 21:37:53 +0530

    [add]+Integer&floating&operators

---
+++ numericData.py
@@ -0,0 +1,30 @@
+num1=10
+
+print(type(num1))
+print('*************************')
+
+num2=-10
+print(abs(num2))#absolute value remove negative number 
+
+print('*************************')
+
+num3=10.4
+print(round(num3))
+
+
+num4=10.48
+print(round(num4,1))
+
+print('*************************')
+
+
+# NOTE: type casting
+
+num5='100'
+num6='200'
+num5=int(num5)
+num6=int(num6)
+print(num5+num6)
+
+num7=int(input('Please nenter the favorite number'))
+print(type(num7))
