12facf0233af551c9f70873cdabf192bb6262c98
vinodsamy <vino929510@gmail.com>
2021-10-26 17:33:37 +0530

    +how to create empty list&tuple&set

---
+++ createEmptyList&Tuple&Set.py
@@ -0,0 +1,10 @@
+lists=[]
+lists=list()
+
+tuples=()
+tuples=tuple()
+
+# NOTE:sets are differne when you creates empty set
+empty_set={}# this is creates dictionary not a set
+empty_set=set()# this is right way to do it.
+
