22c2498ec65d5eb2f3d521d0c936fb14d08d59af
vinodsamy <vino929510@gmail.com>
2021-10-28 14:43:00 +0530

    +Loops And Iterations

---
+++ loopsAndIterations.py
@@ -0,0 +1,47 @@
+nums=[1,2,3,4,5,6]
+
+
+for num in nums:
+  print(num)
+print('********************************************')
+  # NOTE: break and continue statements
+
+# for num in nums:
+  
+#   if num==3:
+#     continue
+#   print('after continue',num)
+
+# for num in nums:
+  
+#   if num==3:
+#     break
+#   print('after continue',num)
+
+# NOTE: Nested loops:
+
+# for num in nums:
+#   for letter in 'abc':
+#     print(num,letter)
+
+
+# NOTE: to get a counter value 
+# for index,num in enumerate(nums):
+#     print(index,num)
+
+# NOTE: using range function
+
+# for i in range(10):
+#   print(i)
+
+# NOTE: custom starting value
+# for i in range(1,11):
+#   print(i)
+
+
+# NOTE:While loop
+
+# x=0;
+# while x<10:
+#   print(x)
+#   x+=1
